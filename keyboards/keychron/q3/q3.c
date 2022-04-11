0376e34005d59f0bf15d4f8daf736c3ee121d1cf
bootstrapper <ido.samuelson@gmail.com>
2022-04-11 18:42:57 -0500

    Added left hand gamer layer, added caps lock

--- keyboards/keychron/q3/q3.c
+++ keyboards/keychron/q3/q3.c
@@ -26,6 +26,8 @@ const matrix_row_t matrix_mask[] = {
     0b1111111111101111,
 };
 
+#ifdef DIP_SWITCH_ENABLE
+
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) { return false;}
     if (index == 0) {
@@ -33,3 +35,37 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
     }
     return true;
 }
+
+#endif
+
+
+// enabling caps lock light
+#if defined(RGB_MATRIX_ENABLE) && defined(CAPS_LOCK_LED_INDEX)
+
+#   define CAPS_LOCK_MAX_BRIGHTNESS 0xff
+#   ifdef RGB_MATGRIX_MAXIMUM_BRIGHTNESS
+#       undef  CAPS_LOCK_MAX_BRIGHTNESS
+#       define CAPS_LOCK_MAX_BRIGHTNESS RGB_MATGRIX_MAXIMUM_BRIGHTNESS
+#endif
+
+#   define CAPS_LOCK_VAL_STEP 8
+#   ifdef RGB_MATRIX_VAL_STEP
+#       undef CAPS_LOCK_VAL_STEP
+#       define CAPS_LOCK_VAL_STEP RGB_MATRIX_VAL_STEP
+#   endif
+
+void rgb_matrix_indicators_kb(void) {
+    if(!host_keyboard_led_state().caps_lock) return ;
+
+    uint8_t b = rgb_matrix_get_val();
+    if (b< CAPS_LOCK_VAL_STEP) {
+        b = CAPS_LOCK_VAL_STEP;
+    } else if (b < (CAPS_LOCK_MAX_BRIGHTNESS - CAPS_LOCK_VAL_STEP)) {
+        b += CAPS_LOCK_VAL_STEP / 4; // one step more than current brightness
+    } else {
+        b = CAPS_LOCK_MAX_BRIGHTNESS;
+    }
+    rgb_matrix_set_color(CAPS_LOCK_LED_INDEX, b, b, b);
+}
+
+#endif
