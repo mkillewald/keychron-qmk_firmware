602cc4b47e891dcefaecaeaaac7c0df7f4f387a4
bootstrapper <ido.samuelson@gmail.com>
2022-04-14 03:54:53 -0500

    fixed right layer color on boot

--- keyboards/keychron/q3/q3.c
+++ keyboards/keychron/q3/q3.c
@@ -31,6 +31,7 @@ const matrix_row_t matrix_mask[] = {
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) { return false;}
     if (index == 0) {
+        layer_state_set(1UL << (active ? 2 : 0));
         default_layer_set(1UL << (active ? 2 : 0));
     }
     return true;
