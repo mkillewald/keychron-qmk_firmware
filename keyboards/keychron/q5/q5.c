e0a567e9d49bd7efeea07c7d5f1e3aac323e76f7
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-06-30 09:32:01 +0800

    Update q5

--- keyboards/keychron/q5/q5.c
+++ keyboards/keychron/q5/q5.c
@@ -32,11 +32,7 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
         return false;
     }
     if (index == 0) {
-#    if defined(OS_SWITCH_REVERT)
-        default_layer_set(1UL << (!active ? 2 : 0));
-#    else
         default_layer_set(1UL << (active ? 2 : 0));
-#    endif
     }
     return true;
 }
--- keyboards/keychron/q5/readme.md
+++ keyboards/keychron/q5/readme.md
@@ -1,6 +1,6 @@
 # Keychron Q5
 
-A customizable 100 keys keyboard.
+A customizable 96% keyboard.
 
 * Keyboard Maintainer: [Keychron](https://github.com/keychron)
 * Hardware Supported: Keychron Q5
