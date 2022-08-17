22ccdc5583a1fcca22b8bd55ab6f5ee0db5e4939
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-08-17 15:36:19 +0900

    Update v8

--- keyboards/keychron/q7/q7_ansi_stm32l432/q7_ansi_stm32l432.c
+++ keyboards/keychron/q7/q7_ansi_stm32l432/q7_ansi_stm32l432.c
@@ -132,4 +132,4 @@ led_config_t g_led_config = {
     }
 };
 
-#endif
+#endif // RGB_MATRIX_ENABLE
--- keyboards/keychron/v8/v8_iso_stm32l432_ec11/v8_iso_stm32l432_ec11.c
+++ keyboards/keychron/v8/v8_iso_stm32l432_ec11/v8_iso_stm32l432_ec11.c
@@ -130,7 +130,7 @@ led_config_t g_led_config = {
     }
 };
 
-#endif
+#endif // RGB_MATRIX_ENABLE
 
 #ifdef ENCODER_ENABLE
 
@@ -146,6 +146,7 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     return true;
 }
 #    if defined(PAL_USE_CALLBACKS)
+
 void encoder0_pad_cb(void *param) {
     (void)param;
 
@@ -160,6 +161,5 @@ void keyboard_post_init_kb(void) {
     palSetLineCallback(encoders_pad_a[0], encoder0_pad_cb, NULL);
     palSetLineCallback(encoders_pad_b[0], encoder0_pad_cb, NULL);
 }
-    #endif
-#endif
-
+#    endif // PAL_USE_CALLBACKS
+#endif     // ENCODER_ENABLE
