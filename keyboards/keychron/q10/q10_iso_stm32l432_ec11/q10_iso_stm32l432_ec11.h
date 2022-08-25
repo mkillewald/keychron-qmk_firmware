65356eae4255744e9bb0bd2542ed39dfb5f3ea8f
lalalademaxiya1 <2831039915@qq.com>
2022-08-25 17:11:06 +0800

    Update iso version of q10

--- keyboards/keychron/q10/q10_iso_stm32l432_ec11/q10_iso_stm32l432_ec11.h
+++ keyboards/keychron/q10/q10_iso_stm32l432_ec11/q10_iso_stm32l432_ec11.h
@@ -21,7 +21,7 @@
 #define XXX KC_NO
 
 // clang-format off
-
+#if 0
 #define LAYOUT_all( \
   K5A, K5B,                                                                              \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E,      K0F, \
@@ -38,6 +38,7 @@
     { K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
     { K50, K51, K52, K53, K54, XXX, XXX, K57, K58, K59, K5A, K5B, XXX, K5D, K5E, K5F }  \
 }
+#endif
 #define LAYOUT_iso_90( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E,      K0F, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E,      K1F, \
