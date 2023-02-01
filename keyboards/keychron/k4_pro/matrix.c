cb53316c05852f66c651784641229bac13851c8b
lokher <lokher@gmail.com>
2023-02-01 16:00:45 +0800

    add k4_pro;add via json files

--- keyboards/keychron/k4_pro/matrix.c
+++ keyboards/keychron/k4_pro/matrix.c
@@ -19,11 +19,11 @@
 #include "gpio.h"
 #include "quantum.h"
 
-#define HC595_STCP B0
+#define HC595_STCP A0
 #define HC595_SHCP A1
-#define HC595_DS A7
+#define HC595_DS C15
 
-#define DIRECT_COL_NUM 1
+#define DIRECT_COL_NUM 2
 
 pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
@@ -31,27 +31,10 @@ pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 static inline void HC595_delay(uint16_t n) {
     while (n-- > 0) {
         asm volatile("nop" ::: "memory");
-    }
+    };
 }
 
-#ifdef DRIVE_SHRIFT_REGISTER_WITH_SPI
-// clang-format off
-const SPIConfig hs_spicfg = {
-    .circular = false,
-    .slave = false,
-    .data_cb = NULL,
-    .error_cb = NULL,
-    .ssport = PAL_PORT(HC595_STCP),
-    .sspad = PAL_PAD(HC595_STCP),
-    .cr1 = SPI_CR1_BR_1,
-    .cr2 = SPI_CR2_DS_3 | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0 | SPI_CR2_SSOE | SPI_CR2_NSSP
-};
-// clang-format on
-#endif
 static void HC595_output(uint16_t data) {
-#ifdef DRIVE_SHRIFT_REGISTER_WITH_SPI
-    spiSend(&SPID1, 1, &data);
-#else
     uint8_t i;
     uint8_t n = 1;
 
@@ -75,7 +58,6 @@ static void HC595_output(uint16_t data) {
     writePinLow(HC595_STCP);
     HC595_delay(n);
     writePinHigh(HC595_STCP);
-#endif
 }
 
 static inline void setPinOutput_writeLow(pin_t pin) {
@@ -102,13 +84,11 @@ static inline uint8_t readMatrixPin(pin_t pin) {
 static bool select_col(uint8_t col) {
     pin_t pin = col_pins[col];
 
-    if (col < 1) {
-        if (pin != NO_PIN) {
-            setPinOutput_writeLow(pin);
-            return true;
-        }
+    if (col < DIRECT_COL_NUM) {
+        setPinOutput_writeLow(pin);
+        return true;
     } else {
-        HC595_output(~(0x01 << (col - 1)));
+        HC595_output(~(0x01 << (col - DIRECT_COL_NUM)));
         return true;
     }
     return false;
@@ -117,22 +97,22 @@ static bool select_col(uint8_t col) {
 static void unselect_col(uint8_t col) {
     pin_t pin = col_pins[col];
 
-    if (col < 1) {
-        if (pin != NO_PIN) {
-            setPinInputHigh_atomic(pin);
-        }
+    if (col < DIRECT_COL_NUM) {
+        setPinInputHigh_atomic(pin);
     } else {
-        if (col >= MATRIX_COLS - 1) HC595_output(0xFFFF);
+        HC595_output(0xFFFF);
     }
 }
 
 static void unselect_cols(void) {
-    if (col_pins[0] != NO_PIN) setPinInputHigh_atomic(col_pins[0]);
+    for (uint8_t i = 0; i < DIRECT_COL_NUM; i++)
+        writePinHigh(col_pins[i]);
     HC595_output(0xFFFF);
 }
 
 void select_all_cols(void) {
-    if (col_pins[0] != NO_PIN) setPinOutput_writeLow(col_pins[0]);
+    for (uint8_t i = 0; i < DIRECT_COL_NUM; i++)
+        setPinOutput_writeLow(col_pins[i]);
     HC595_output(0x0000);
 }
 
@@ -167,16 +147,9 @@ void matrix_init_custom(void) {
             setPinInputHigh_atomic(row_pins[x]);
         }
     }
-#ifdef DRIVE_SHRIFT_REGISTER_WITH_SPI
-    palSetPadMode(PAL_PORT(HC595_SHCP), PAL_PAD(HC595_SHCP), PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST); /* SCK */
-    palSetPadMode(PAL_PORT(HC595_DS), PAL_PAD(HC595_DS), PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);     /* MOSI*/
-    palSetPadMode(PAL_PORT(HC595_STCP), PAL_PAD(HC595_STCP), PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST); /* CS*/
-    spiStart(&SPID1, &hs_spicfg);
-#else
     setPinOutput(HC595_DS);
     setPinOutput(HC595_STCP);
     setPinOutput(HC595_SHCP);
-#endif
     unselect_cols();
 }
 
