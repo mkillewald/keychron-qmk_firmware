afa7780bbfa74f36ff52c272dbb108812920b644
Mike Killewald <mkillewald@yahoo.com>
2023-02-19 20:28:07 -0500

    moved typedef from .h to .c

--- keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/mkillewald/keymap_user_config.c
+++ keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/mkillewald/keymap_user_config.c
@@ -17,6 +17,16 @@
 #include QMK_KEYBOARD_H
 #include "keymap_user_config.h"
 
+typedef union {
+  uint32_t raw;
+  struct {
+    bool caps_lock_light_tab :1;
+    bool caps_lock_light_alphas :1;
+    bool fn_layer_transparent_keys_off :1;
+    bool fn_layer_color_enable :1;
+  };
+} user_config_t;
+
 user_config_t user_config;
 
 void eeconfig_init_user(void) {
--- keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/mkillewald/keymap_user_config.h
+++ keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/mkillewald/keymap_user_config.h
@@ -14,17 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  
- #pragma once
- 
- typedef union {
-  uint32_t raw;
-  struct {
-    bool caps_lock_light_tab :1;
-    bool caps_lock_light_alphas :1;
-    bool fn_layer_transparent_keys_off :1;
-    bool fn_layer_color_enable :1;
-  };
-} user_config_t;
+#pragma once
 
 void user_config_read(void);
 void user_config_write(void);
