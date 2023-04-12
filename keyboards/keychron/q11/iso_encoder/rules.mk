38fdb3190242c4eabaf427aaada2926e09d4fd05
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-04-12 14:54:29 +0800

    Add q11 iso version

---
+++ keyboards/keychron/q11/iso_encoder/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+SERIAL_DRIVER = usart
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
