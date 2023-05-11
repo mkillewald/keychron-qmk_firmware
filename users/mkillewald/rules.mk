b9cbece98d21aade1123da71c4ccbb13a3c899d5
Mike Killewald <mkillewald@yahoo.com>
2023-05-11 01:29:46 -0400

    added userspace folder

---
+++ users/mkillewald/rules.mk
@@ -0,0 +1,10 @@
+RAW_ENABLE = no
+VIA_ENABLE = yes
+
+SRC += mkillewald.c
+SRC += eeprom_user_config.c
+SRC += myfork_keychron_ft.c
+
+ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+    SRC += rgb_matrix_user.c
+endif
